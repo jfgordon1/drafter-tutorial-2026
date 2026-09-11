bb338c71f69ff9cbec5fccb800fd2327ba3b436a
jfgordon1 <149754136+jfgordon1@users.noreply.github.com>
2026-09-11 00:08:13 -0400

    added initial code

--- main.py
+++ main.py
@@ -2,22 +2,53 @@ from bakery import assert_equal
 from drafter import *
 from dataclasses import dataclass
 
-
-
-hide_debug_information()
-set_website_title("Your Drafter Website")
-set_website_framed(False)
-
-
 @dataclass
 class State:
-    pass
-
+    count: int
 
 @route
 def index(state: State) -> Page:
-    return Page(state, ["Hello World!"])
+    return Page(state, [
+        "Current count: " + str(state.count) + "\n",
+        Button("+1", "increment"),
+        Button("-1", "decrement"),
+        Button("+10", "increment_10"),
+        Button("Reset", "reset_count")
+        ])
 
-assert_equal(index(State()), Page(State(), ["Hello World!"]))
+@route
+def increment(state: State) -> Page:
+    state.count = state.count + 1
+    return index(state)
 
-start_server(State())
+@route
+def increment_10(state: State) -> Page:
+    state.count = state.count + 10
+    return index(state)
+
+@route
+def decrement(state: State) -> Page:
+    state.count = state.count - 1
+    return index(state)
+
+@route
+def reset_count(state: State) -> Page:
+    state.count = 0
+    return index(state)
+
+assert_state(increment(State(0)), State(1))
+assert_state(reset_count(State(7)), State(0))
+assert_has(index(State(3)), "Current count: 3")
+
+set_website_title("My First Counter")
+set_site_information(
+    author="Jacob Gordon",
+    description="Counter increases and decreases when you click corresponding button.",
+    sources="Thanks to the Drafter docs and my study group.",
+    planning="plan.pdf",
+    links=["https://github.com/"]
+)
+hide_debug_information()
+set_website_framed(False)
+
+start_server(State(0))
